d09fa463db2cc69877b06710d5c7d35fb8df8858
mauricioah96 <82173764+mauricioah96@users.noreply.github.com>
2021-05-06 12:50:06 -0500

    Add files via upload

---
+++ BootcampPractice.DTO/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BootcampPractice.DTO
+{
+    //El DTO tiene cosas que puedes exponer. No puedes poner un password, por ejemplo.
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
